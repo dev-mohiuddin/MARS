0c8cf88e01f353c29b2ae845856a2ab65650c8d5
unknown <mohiuddin.mern.dev@gmail.com>
2025-01-26 00:21:46 +0600

    up

--- src/components/layout/sidebar/nav-user.jsx
+++ src/components/layout/sidebar/nav-user.jsx
@@ -82,10 +82,6 @@ export function NavUser () {
             Account
           </DropdownMenuItem>
           <DropdownMenuItem>
-            <CreditCard />
-            Billing
-          </DropdownMenuItem>
-          <DropdownMenuItem>
             <Bell />
             Notifications
           </DropdownMenuItem>
